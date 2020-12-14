7a5efd077e997f34aacb9909b373d06aa04ce6e4
abrarnaim015 <aabrarnaim24@gmail.com>
2020-12-14 12:12:43 +0700

    Add Footer Adopt.Us

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,12 +6,12 @@ function FooterComponent(props) {
   const history = useHistory()
 
   return (
-    <div style={{ backgroundColor: '#B43D66' }} className="boxFooter text-center p-3 d-flex justify-content-between">
+    <div className="boxFooter text-center p-3 d-flex justify-content-between">
       <div>
         <img style={{ height: '30px', marginLeft: '10px' }} src={'./logoName.png'} alt={'logo Adopt.Us'}></img>
       </div>
       <div>
-        <h5 className="mt-1 text-light my-0">©2020 Adopt.Us</h5>
+        <h5 style={{ color: '#B43D66' }} className="mt-1 my-0">©2020 Adopt.Us</h5>
       </div>
       <div>
         <img style={{ width: '30px', marginRight: '10px' }} src={'https://image.flaticon.com/icons/png/512/25/25231.png'} alt={'github'}></img>
